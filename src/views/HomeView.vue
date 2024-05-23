8314c11f49f844f7c16824eb8def918ee9925753
lukaswarren <lukaswarren728@gmail.com>
2024-05-23 11:48:26 -0600

    Login changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,55 @@
 <script setup>
-import { ref } from "vue";
+import { ref , onMounted} from "vue";
+import axios from "axios";
 import PlacesMap from "../components/PlacesMap.vue"
 import PlacesUpload from "../components/PlaceUpload.vue"
+import LoginComponent from "../components/LoginComponent.vue"
 
 const showModal = ref(false)
+
+const isAuthenticated = ref(false);
+
+const getSession = async () => {
+  try {
+    const response = await axios.get("http://localhost:8000/places-api/session/", { withCredentials: true });
+    isAuthenticated.value = response.data.isAuthenticated;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const handleLoginSuccess = () => {
+  isAuthenticated.value = true;
+};
+
+const logout = async () => {
+  try {
+    await axios.get("http://localhost:8000/places-api/logout/", { withCredentials: true });
+    isAuthenticated.value = false;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+onMounted(() => {
+  getSession();
+});
 </script>
 
 <template>
-  <button id="show-modal" @click="showModal = true">Upload</button>
-  <Teleport to="body">
-    <!-- use the modal component, pass in the prop -->
-    <PlacesUpload :show="showModal" @close="showModal = false">
-      <template #header>
-        <h3>Upload</h3>
-      </template>
-    </PlacesUpload>
-  </Teleport>
-  <PlacesMap/> 
+  <LoginComponent v-if="!isAuthenticated" @loginSuccess="handleLoginSuccess" />
+  <div v-else>
+    <button id="show-modal" @click="showModal = true">Upload</button>
+    <Teleport to="body">
+      <!-- use the modal component, pass in the prop -->
+      <PlacesUpload :show="showModal" @close="showModal = false">
+        <template #header>
+          <h3>Upload</h3>
+        </template>
+      </PlacesUpload>
+    </Teleport>
+    <PlacesMap/>
+  </div>
 </template>
 <style scoped>
   button {
